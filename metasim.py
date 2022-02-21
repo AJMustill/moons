6b8bd360e8472c39074356bff552e66532569dbf
AJMustill <alex@astro.lu.se>
2022-02-21 13:18:16 +0100

    Add metasim.py

---
+++ metasim.py
@@ -0,0 +1,21 @@
+import globals
+
+class MetaSim:
+    
+    def __init__(self):
+        
+        
+        return
+    
+        
+    def rewind(self,t=1):
+        
+        
+        
+        return
+    
+    
+    def add_moons(self):
+        
+        
+        return
